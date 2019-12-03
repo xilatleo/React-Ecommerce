892c5372b595e26380b5c3a273d88715f82e4c56
xilatleo <trinhhoangquan234@gmail.com>
2019-12-03 16:01:46 -0600

    added Router Dom to each section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component'
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
 
+const HatPage = () => (
+  <di>
+    <h1>Hats page</h1>
+  </di>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatPage} />
+      </Switch>
     </div>
   );
 }
