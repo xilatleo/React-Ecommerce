fddbb7f320bda61ce7a93ac4422e4e92d5e9173e
xilatleo <trinhhoangquan234@gmail.com>
2019-12-04 22:17:17 -0600

    Added Signup with Email and Password

--- src/pages/signin-and-signup/signin-and-signup.component.jsx
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 import './signin-and-signup.style.scss'
 import SignIn from '../../components/sign-in/sign-in.component'
+import SignUp from '../../components/sign-up/sign-up.component'
 
 const SignInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
 )
 export default SignInAndSignUpPage
