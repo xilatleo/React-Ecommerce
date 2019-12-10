87e6748109d63456fc5fb490bd75a7fdf4d93a0b
xilatleo <trinhhoangquan234@gmail.com>
2019-12-09 20:50:46 -0600

    Added CartItem and get item displayed after clicking addtocart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import CustomButton from '../custom-button/custom-button.component'
+import React from "react";
+import { connect } from "react-redux";
 
-import './cart-dropdown.style.scss';
+import CustomButton from "../custom-button/custom-button.component";
+import CartItem from "../cart-item/cart-item.component";
 
-const CartDropdown = () => (
-    <div className='cart-dropdown'>
-        <div className='cart-items'>
-            <CustomButton>GO TO CHECKOUT</CustomButton>
-        </div>
+import "./cart-dropdown.style.scss";
+
+const CartDropdown = ({ cartItems }) => (
+  <div className="cart-dropdown">
+    <div className="cart-items">
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
     </div>
-)
-export default CartDropdown;
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
+);
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
